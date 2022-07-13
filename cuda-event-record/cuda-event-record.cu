6841663f8eb45d55d0ef07af2e5fd5cbba22c0a1
Ze Sheng <z7sheng@uwaterloo.ca>
2022-07-13 16:19:05 +0000

    Updated Experiment

--- cuda-event-record/cuda-event-record.cu
+++ cuda-event-record/cuda-event-record.cu
@@ -2,6 +2,7 @@
 #include <vector>
 #include <mutex>
 #include <condition_variable>
+#include <thread>
 
 #include "cuda_runtime.h"
 
@@ -54,32 +55,54 @@ void CUDART_CB block_op_host_fn(void* arg) {
 
 static cudaEvent_t end_event;
 
-int main() {
-  cudaStream_t stream1, stream2;
-  CUDACHECK(cudaStreamCreate(&stream1));
-  CUDACHECK(cudaStreamCreate(&stream2));
+// check if other threads are non-blocking
+static void checkOtherThread() {
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+  for (int i = 0; i < 10; i++) {
+    std::cout << "Non-blocking" << i << std::endl;
+  }
+}
 
+// keep launching empty kernels
+static void launchKernels(cudaStream_t stream) {
+  for (int i = 0; i < 1000; i++) {
+    NoOpKernel<<<1, 128, 0, stream>>>();
+    std::cout << "Done KernelLaunch #" << i << std::endl;
+  }
+}
+
+static void blocker(cudaStream_t stream) {
   int num_events = 150; // hangs if THIS >= 57
   std::vector<std::shared_ptr<Event>> event_vec;
 
   for (int i = 0; i < num_events; i++) {
-    NoOpKernel<<<1, 128, 0, stream1>>>();
-
     event_vec.push_back(std::make_shared<Event>());
-    cudaLaunchHostFunc(stream1, block_op_host_fn, event_vec.back().get());
+    cudaLaunchHostFunc(stream, block_op_host_fn, event_vec.back().get());
 
     std::cout << "Before recording #" << i << std::endl;
     CUDACHECK(cudaEventCreate(&end_event));
-    CUDACHECK(cudaEventRecord(end_event, stream1));
-    // CUDACHECK(cudaEventRecord(end_event, stream2));
+    CUDACHECK(cudaEventRecord(end_event, stream));
     std::cout << "After recording  #" << i << std::endl;
-
-    // NoOpKernel<<<1, 128, 0, stream1>>>();
   }
 
   for (int i = 0; i < num_events; i++) {
     event_vec[i]->Signal();
   }
+}
+
+
+int main() {
+  cudaStream_t stream1, stream2;
+  CUDACHECK(cudaStreamCreate(&stream1));
+  CUDACHECK(cudaStreamCreate(&stream2));
+
+  auto t1 = std::thread(blocker, stream1);
+  auto t2 = std::thread(launchKernels, stream2);
+  auto t3 = std::thread(checkOtherThread);
+
+  t1.join();
+  t2.join();
+  t3.join();
 
   // clean up
   CUDACHECK(cudaDeviceSynchronize());
