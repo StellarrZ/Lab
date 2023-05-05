5a86d07d9435a4a0c9b8bf883fc067893887089f
Ze Sheng <z7sheng@uwaterloo.ca>
2023-05-05 20:09:52 +0000

    Experiment std::advance

--- std/iterator.cpp
+++ std/iterator.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+#include <vector>
 #include <iterator>
 #include <map>
 
 
 int main() {
   std::map<int, int> m;
-  for (int i; i < 5; i++) {
+  for (int i = 0; i < 5; i++) {
     m.emplace(i, i);
   }
 
   auto it = m.begin();
-  // Supported from g++ 10 (--std=c++2a)
-  auto n = std::ranges::next(it, 3);
-  std::cout << *n.second << std::endl;
+  // // Supported from g++ 10 (--std=c++2a)
+  // auto n = std::ranges::next(it, 3);
+  // std::cout << *n.second << std::endl;
+  
+  std::cout << (*it).second << std::endl;
+
+  std::advance(it, 2);
+  std::cout << it->second << std::endl;
+
+  return 0;
 }
+
