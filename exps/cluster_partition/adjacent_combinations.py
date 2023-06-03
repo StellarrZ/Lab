17298ef12b6a97f69aaad5f460b08a51a997d66d
Ze Sheng <z7sheng@uwaterloo.ca>
2023-06-03 02:08:19 +0000

    Exp finding Euler path

--- exps/cluster_partition/adjacent_combinations.py
+++ exps/cluster_partition/adjacent_combinations.py
@@ -1,46 +1,70 @@
 from itertools import combinations as comb
 from itertools import permutations as perm
+from collections import deque, Counter, defaultdict
+import heapq
 
 
-def main():
-  num_partitions = 6
-
-  pairs = list(comb(range(num_partitions), 2))
-  print(pairs)
-  print()
-
-  i = 0
-  while i + 1 < len(pairs):
-    if pairs[i + 1][0] in pairs[i] or pairs[i + 1][1] in pairs[i]:
-      i += 1
-    else:
-      pairs = pairs[:i + 1] + pairs[i + 2:] + [pairs[i + 1]]
-
-  print(pairs)
+def verifier(fn):
+  def decorated_fn():
+    res = fn()
+    l = list(map(lambda x: tuple(sorted(x)), res)) + list(comb(range(num_partitions), 2))
+    # print(Counter(l))
+    assert (max(Counter(l).values()) == min(Counter(l).values()) == 2), "FAIL"
+    print("PASS")
+    return res
+  
+  return decorated_fn
 
 
-  # pairs = list(perm(range(num_partitions), 2))
-  # print(pairs)
-  # print()
+# @verifier
+# def heuristic():
+#   res = []
+#   credit = [set([j for j in range(num_partitions - 1, -1, -1) if j != i]) for i in range(num_partitions)]
+#   cur = 0
+#   while len(res) < num_partitions * (num_partitions - 1) / 2:
+#     opt_idx, max_val = -1, float("-inf")
+#     for j in credit[cur]:
+#       if len(credit[j]) > max_val:
+#         opt_idx, max_val = j, len(credit[j])
+#     res.append((cur, opt_idx))
+#     try:
+#       credit[cur].remove(opt_idx)
+#       credit[opt_idx].remove(cur)
+#     except:
+#       print("=== except === ", res)
+#       return res
+#     cur = opt_idx
+  
+#   return res
 
-  # repo = set()
 
-  # i = 0
-  # result_len = len(pairs) // 2
-  # while i + 1 < result_len:
-  #   if pairs[i] in repo:
-  #     pairs = pairs[:i] + pairs[i + 1:]
-  #   elif pairs[i + 1][0] == pairs[i][1]:
-  #     print(pairs[i])
-  #     repo.add((pairs[i][1], pairs[i][0]))
-  #     i += 1
-  #   else:
-  #     pairs = pairs[:i + 1] + pairs[i + 2:] + [pairs[i + 1]]
+@verifier
+def euler_path_traverse():
+  res = []
+  # adj-lists
+  graph = [set([j for j in range(num_partitions) if j != i]) for i in range(num_partitions)]
+  pre = 0
+  # dfs
+  stack = list(graph[0])
+  while len(res) < num_partitions * (num_partitions - 1) / 2:
+    cur = stack.pop()
+    res.append((pre, cur))
+    graph[pre].remove(cur)
+    graph[cur].remove(pre)
+    stack += graph[cur]
+    pre = cur
 
-  # print(result_len, pairs[:result_len])
-  # print(len(list(comb(range(num_partitions), 2))))
+  return res
 
 
-
+""" 
+  To contain Euler path
+  must has odd number of vertices
+  so that there is no vertex has odd degree
+"""
 if __name__ == '__main__':
-  main()
+
+  for num_partitions in range(3, 100, 2):
+    print("\nnum_partitions = %d" % num_partitions)
+    res = euler_path_traverse()
+    # print(res, len(res))
